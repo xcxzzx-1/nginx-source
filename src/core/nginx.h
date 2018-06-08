ede49d244085b6f700671729645a4b19e8aebf8c
root <root@localhost>
2018-06-08 07:45:33 -0700

    修改nginx版本号

--- src/core/nginx.h
+++ src/core/nginx.h
@@ -10,8 +10,9 @@
 
 //修改nginx版本号，禁止显示版本号
 #define nginx_version      1012002
-#define NGINX_VERSION      "1.12.2"
-#define NGINX_VER          "nginx/" NGINX_VERSION
+//#define NGINX_VERSION      "1.12.2"
+#define NGINX_VERSION      "8.8.8"
+#define NGINX_VER          "mine-nginx/" NGINX_VERSION
 
 #ifdef NGX_BUILD
 #define NGINX_VER_BUILD    NGINX_VER " (" NGX_BUILD ")"
